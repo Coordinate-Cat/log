56fe5c46d40ca9fc3f0c39733e1cd75cc91e1d3f
Coordinate-Cat <tetrapasta02@gmail.com>
2024-12-19 14:51:35 +0900

    Oramaライブラリの依存関係を削除し、ナビゲーションバーにGitHubリンクを追加。不要なブログ検索関連のファイルを削除。

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -3,9 +3,35 @@ import "../../global.css";
 
 export const Navbar = () => {
   return (
-    <div className="mt-4" role="navigation" aria-label="main navigation">
+    <div
+      className="mt-4 flex items-center justify-between"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <a href={"/"} className="text-white no-underline">
         <h1 className="m-0 w-fit bg-white p-1 text-black">.log</h1>
+      </a>
+      {/* github */}
+      <a
+        href={"https://github.com/Coordinate-Cat"}
+        className="m-0 text-white no-underline"
+        target="blank"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-github"
+        >
+          <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+          <path d="M9 18c-4.51 2-5-2-7-2" />
+        </svg>
       </a>
     </div>
   );
