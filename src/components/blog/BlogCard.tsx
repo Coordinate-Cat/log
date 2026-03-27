94e73f9025ecd11212820677585e1e287f57787b
Coordinate-Cat <tetrapasta02@gmail.com>
2026-03-27 09:59:45 +0900

    refactor: Simplify BlogCard component layout and improve styling consistency

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -19,16 +19,13 @@ export const BlogCard: React.FC<Props> = ({
   return (
     <a href={slug} className="w-full text-white no-underline hover:text-white">
       <div className="m-0 flex">
-        <div className="m-0 w-fit truncate border-b-2 border-white bg-none p-1 leading-tight">
+        <div className="m-0 truncate bg-transparent p-1 leading-tight">
           {title}
         </div>
         <div className="m-0 flex-1 bg-white"></div>
-      </div>
-      <div className="m-0 flex">
-        <p className="bold m-0 w-fit text-nowrap border-r-2 border-white p-1 pr-2 italic">
+        <p className="bold m-0 shrink-0 text-nowrap border-b-2 border-l-2 border-white bg-transparent p-1 pl-2 italic">
           {getFormattedDate(date)}
         </p>
-        <div className="m-0 flex-1 bg-white"></div>
       </div>
       {/* <div className="m-0 flex">
         <p className="mb-0 w-fit truncate border-t-2 border-white p-1">
